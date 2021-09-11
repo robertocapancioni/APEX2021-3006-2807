dc93465d25fdd54dcbaecf4f83ccf5c4bc906967
Roberto Capancioni <roberto.capancioni@gmail.com>
2021-09-11 23:33:48 +0200

    Update select_portale.sql

--- d50/select_portale.sql
+++ d50/select_portale.sql
@@ -20,3 +20,22 @@ select distinct u.application_id CARD_INITIALS,
    and  u.application_id = s.application_id
    and s.SUBSTITUTION_STRING = 'APP_ICON_CLASS'
  where user_name=v('APP_USER');
+ 
+ -- se si aggiunge la condivisione di sessione co ni coockies
+ select distinct u.application_id CARD_INITIALS,
+                u.application_name card_title,
+                'f?p=' || u.application_id || ':1:'||v('APP_SESSION')CARD_LINK,
+                nvl(SUBSTITUTION_VALUE,'fa-apex')card_icon,
+                null card_text,
+                null card_subtext 
+  from apex_appl_acl_user_roles u
+  left join APEX_APPLICATION_SUBSTITUTIONS s 
+         on u.workspace = s.workspace
+        and u.application_id = s.application_id
+        and s.SUBSTITUTION_STRING = 'APP_ICON_CLASS'
+  join      APEX_APPLICATION_AUTH a
+         on u.workspace = a.workspace
+        and u.application_id = a.application_id
+        and is_current_authentication='Y'
+        and COOKIE_NAME ='PORT01'
+ where user_name=v('APP_USER');
